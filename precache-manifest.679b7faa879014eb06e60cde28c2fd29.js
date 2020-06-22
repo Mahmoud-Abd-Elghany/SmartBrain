71c2cccfefc12025f759ae9df50e1cfb7e45ba9b
unknown <mazpc97@gmail.com>
2020-06-22 18:03:31 +0200

    Updates

---
+++ precache-manifest.679b7faa879014eb06e60cde28c2fd29.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b11fb04bc144371e619fe82ee7d94fc4",
+    "url": "/SmartBrain/index.html"
+  },
+  {
+    "revision": "f9134e874df11407f93f",
+    "url": "/SmartBrain/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "e99fc53c64d7b2580cc3",
+    "url": "/SmartBrain/static/css/main.bade8f60.chunk.css"
+  },
+  {
+    "revision": "f9134e874df11407f93f",
+    "url": "/SmartBrain/static/js/2.750726b6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/SmartBrain/static/js/2.750726b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e99fc53c64d7b2580cc3",
+    "url": "/SmartBrain/static/js/main.d85cb75e.chunk.js"
+  },
+  {
+    "revision": "405698958a3472004a72",
+    "url": "/SmartBrain/static/js/runtime-main.db396b0b.js"
+  }
+]);
