0d4edbfb2e86ae7545581adbd0eb683b0276b400
unknown <mazpc97@gmail.com>
2020-06-23 16:01:23 +0200

    imagebox functionality

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
 
 function App() {
+  const [image, imageChange] = useState("");
+
+  useEffect(()=>{
+    console.log(image)
+  },[image])
+
+  const submitHandler = () => {
+    console.log("Done");
+  }
+  
   return (
     <div>
         <Navigation/>
         <Logo/>
         <Rank/>
-        <ImageLinkForm/>
+        <ImageLinkForm changeHandler={imageChange} submitHandler={submitHandler}/>
 
     </div>
   );
--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -1,14 +1,18 @@
 import React from "react";
 import "./ImageLinkForm.css"
 
-const ImageLinkForm = () => {
+const ImageLinkForm = ({submitHandler, changeHandler}) => {
     return(
         <div>
             <p className="f4 tc white">This app detects the face in the picture</p>
             <div className="center">
                 <div className="pa3 br-5 shadow-5 wid">
-                    <input type="text" className="f4 w-70 pa2 link" />
-                    <button className="w-25 f4 grow link ph3 dib white bg-light-purple flex-auto but ml3">detect</button>
+
+                    <input type="text" className="f4 w-70 pa2 link" onChange={event => changeHandler(event.target.value)}/>
+
+                    <button className="w-25 f4 grow link ph3 dib white bg-light-purple flex-auto but ml3" 
+                    onClick={submitHandler}>detect</button>
+                    
                 </div>
             </div>
             
